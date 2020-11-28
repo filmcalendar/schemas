f471feabf03646dfe86dfaae5039ecaddbac16c7
Rui Costa <ruicosta042@gmail.com>
2020-11-28 12:03:14 +0000

    feat: go back to cjs bundle format

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 rimraf dist
-microbundle --output dist/index.js  --no-pkg-main --target node --sourcemap false --format esm
+microbundle --output dist/index.js  --no-pkg-main --target node --sourcemap false --format cjs
