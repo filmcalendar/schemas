c9a948046d65af1411978b17bc083b96a533914d
Rui Costa <ruicosta042@gmail.com>
2020-12-09 18:58:41 +0000

    feat: cinema, live-tv and streaming properties

---
+++ src/films/index.ts
@@ -0,0 +1,19 @@
+import availability from './availability.def-schema.json';
+import film from './film.def-schema.json';
+import page from './page.def-schema.json';
+import provider from './provider.def-schema.json';
+import schema from './films.schema.json';
+import session from './session.def-schema.json';
+import sessionAttribute from './session-attribute.def-schema.json';
+
+export default {
+  ...schema,
+  definitions: {
+    availability,
+    film,
+    page,
+    provider,
+    session,
+    sessionAttribute,
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-import cinemas from './cinemas';
+import films from './films';
 
-export default { cinemas };
+export default { films };
